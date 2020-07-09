78822e6875da124b35dd1d2b970c1ae1ea535fa0
Enol <grimgaiden@gmail.com>
2020-07-09 13:43:39 +0200

    Added one more restriction to 'modelAndView' validation in 'HorarioInterceptor.java'

--- src/main/java/com/bolsadeideas/springboot/app/springboothorario/interceptors/HorarioInterceptor.java
+++ src/main/java/com/bolsadeideas/springboot/app/springboothorario/interceptors/HorarioInterceptor.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -44,7 +45,7 @@ public class HorarioInterceptor implements HandlerInterceptor {
             ModelAndView modelAndView) throws Exception {
 
         String mensaje = (String) request.getAttribute("mensaje");
-        if(modelAndView != null) {
+        if(modelAndView != null && handler instanceof HandlerMethod) {
             modelAndView.addObject("horario", mensaje);
         }
     }
